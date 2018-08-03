116ccd760b2c942609fe79b50d46027bd76c0392
ronanwow1001 <ronancaleb123@gmail.com>
2018-08-03 12:55:32 -0700

    Update game_no_input.bat

--- game_no_input.bat
+++ game_no_input.bat
@@ -1,1 +1,31 @@
+echo off
+cd..
 
+title THE TTM ENGINE
+
+set TTI_PLAYCOOKIE=User
+set TTI_GAMESERVER=127.0.0.1
+set ttiUsername=username
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+rem Get the user input:
+set /P ttiUsername="Username: "
+title RTTIM Client (%ttiUsername%)
+set /P TTI_GAMESERVER="Gameserver (DEFAULT: localhost): " || ^
+
+rem Export the environment variables:
+set TTI_PLAYCOOKIE=%ttiUsername%
+
+echo ===============================
+echo Starting Toontown...
+echo ppython: %PPYTHON_PATH%
+echo Username: %ttiUsername%
+echo Gameserver: %TTI_GAMESERVER%
+echo ===============================
+
+%PPYTHON_PATH% -m toontown.toonbase.ClientStart
+pause
+
+goto server
