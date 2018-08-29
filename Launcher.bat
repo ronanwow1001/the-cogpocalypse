0791f7497a508d0e2fa8b256cbb3e76c414cd5db
ronanwow1001 <ronancaleb123@gmail.com>
2018-08-28 17:26:39 -0700

    Update Launcher.bat

--- Launcher.bat
+++ Launcher.bat
@@ -4,27 +4,5 @@ title THE TTM ENGINE
 
 set TTI_PLAYCOOKIE=User
 set TTI_GAMESERVER=127.0.0.1
-set ttiUsername=username
 
-rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
-set /P PPYTHON_PATH=<PPYTHON_PATH
-
-rem Get the user input:
-set /P ttiUsername="Username: "
-title TTM Client (%ttiUsername%)
-set /P TTI_GAMESERVER="Gameserver (DEFAULT: localhost): " || ^
-
-rem Export the environment variables:
-set TTI_PLAYCOOKIE=%ttiUsername%
-
-echo ===============================
-echo Starting Toontown...
-echo ppython: %PPYTHON_PATH%
-echo Username: %ttiUsername%
-echo Gameserver: %TTI_GAMESERVER%
-echo ===============================
-
-%PPYTHON_PATH% -m toontown.toonbase.ClientStart
-pause
-
-goto server
+ppython.exe "launcher.py"
